06dc4303fd8113a60c3a14ed18c4b2010e2e24af
prince kumar <princerajpit5868@gmail.com>
2026-07-17 05:08:12 +0530

    fix certificate model casing issues imports

--- controllers/adminCertificatesController.js
+++ controllers/adminCertificatesController.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 // import certificate from "../models/certificate.js";
 import Course from "../models/Course.js";
 import CourseEnrollment from "../models/CourseEnrollment.js";
-import Certificate from "../models/certificate.js";
+import Certificate from "../models/Certificate.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
